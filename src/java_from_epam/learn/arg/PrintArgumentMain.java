d85d2c736c5863061521ec071db8bcec82d4878c
evgen-jkay <evgenlandarenko@gmail.com>
2022-12-08 19:42:36 +0200

    new

--- src/java_from_epam/learn/arg/PrintArgumentMain.java
+++ src/java_from_epam/learn/arg/PrintArgumentMain.java
@@ -3,7 +3,7 @@ package java_from_epam.learn.arg;
 public class PrintArgumentMain {
     public static void main(String[] args) {
         for (String str : args) {
-            System.out.println("Argument--> %s%n", str);
+            System.out.println("Argument--> %s%n");
         }
     }
 }
